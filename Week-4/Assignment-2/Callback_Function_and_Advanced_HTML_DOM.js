a23a437431fcd7f403cae353ca0005c9b1a90c1f
Yu-Chieh Liao <tina5ps93@gmail.com>
2022-07-01 21:00:31 +0800

    revise

--- Week-4/Assignment-2/Callback_Function_and_Advanced_HTML_DOM.js
+++ Week-4/Assignment-2/Callback_Function_and_Advanced_HTML_DOM.js
@@ -19,8 +19,7 @@ function render(data){
         let phone = document.createTextNode(element);
         ele.appendChild(phone); 
     });
-    document.body.insertBefore(ele);
-    
+    document.body.prepend(ele); 
 }
 
 ajax(
